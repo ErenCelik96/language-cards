02e1af8050ae4bd81b62295863bfa1561ecaaa1b
Erencan ÇELİK <celik.erenn96@gmail.com>
2021-08-12 17:02:10 +0300

    tasarım tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from "./components/header/Header";
+import Card from "./components/card/Card";
 
 function App() {
   return (
     <div className="App">
-     
+     <Header/>
+     <Card/>
     </div>
   );
 }
---
+++ src/components/header/Header.js
@@ -0,0 +1,12 @@
+import "../header/Header.css";
+
+export default function header() {
+    return (
+        <div className="header">
+            <div className="header--container">
+                {/*image area*/}
+            </div>
+        </div>
+    )
+}
+
